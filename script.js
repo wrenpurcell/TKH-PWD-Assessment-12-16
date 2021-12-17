e6d5fc29645b6b040683c4e34e426814ef409493
Jenny <jennyd.moncion@gmail.com>
2021-12-16 19:56:53 -0500

    test 2

--- script.js
+++ script.js
@@ -5,10 +5,14 @@
 */
 function fiveCharactersOrFewerOnly(arr) {
   // your code here
+  return arr.filter(function(newarr) { 
+    return newarr.length <= 5;
+  });
 }
 // test
 console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"])); 
 //expected output: ["by", "dog", "wolf", "eaten"]
+
 
 /* 2.
 * Make a function that takes a string and returns true if the string could be a 
@@ -16,7 +20,12 @@ console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "
 */
 function isNum(str) {
 //your code here 
-}
+  if (isNaN(str)) {
+    return "false"
+  } else {
+    return "true"
+  };
+};
 //test
 console.log(isNum("3"))
 //expected output: true 
@@ -24,11 +33,21 @@ console.log(isNum("3"))
 console.log(isNum("three"))
 //expected output: false
 
+
 /*3
 * Make a function that takes an array and returns the average of the array. 
 */
+arr = []
 function averageArray(arr) {
   //your code here 
+  let sum = 0,
+        avg = 0;
+    for (let i of x ){
+        sum = sum + i;
+        avg = sum/x.length;
+    }
+    
+    return avg
 }
 //test
 console.log(averageArray[1,2,3])
@@ -37,12 +56,15 @@ console.log(averageArray[1,2,3])
 console.log(averageArray[5, -10, 10, 20])
 //expected output 6.25
 
+
 /* 4.
 *Using Reduce,
 *Turn an array of numbers into a string of the numbers.
 */
 function stringConcat(arr) {
    // your code here 
+  const str = arr.join("");
+  return str;
 }
 //test
 console.log(stringConcat([1,2,3])); 
@@ -54,7 +76,11 @@ console.log(stringConcat([1,2,3]));
 * and reverses it 
 */
 function reverseString(str){
-
+  if (str === "") {
+    return "";
+} else {
+    return reverseString(str.substr(1)) + str.charAt(0);
+}
 }
 //test
 console.log(reverseString('banana'))
@@ -65,16 +91,14 @@ console.log(reverseString('banana'))
 * OOP has 4 pillars and we learned about each in this phase. 
 * for each question - answer which pillar relates to the statement 
 *    A. The process by which one object can acquire the properties of another object 
-*        A. Answer: your answer here 
+*        A. Answer: INHERITANCE 
 *    B. To hide away implementation details inside a function, so when you call the function you don't have to understand exactly what it is doing
-*        B. Answer: your answer here 
+*        B. Answer: ABSTRACTION
 *    C. The action of enclosing something in its own container of related data 
-*        C. Answer: your answer here 
+*        C. Answer: ENCAPSULATION
 *     D. When a child class has its own unique behavior but still shares the same 
 * methods or behaviors of its parent's class 
-*        D. Answer: your answer here 
-
-
+*        D. Answer: POLYMORPHISM
 
 
 /*  7.
@@ -82,6 +106,17 @@ console.log(reverseString('banana'))
  */
 function booleanToString(bool){
   //your code here
+  switch(bool) { 
+    case true: 
+    case "true": 
+    case 1: 
+    case "1": 
+    case "on": 
+    case "yes": 
+      return true; 
+    default: 
+      return false; 
+  } 
 }
 //test
 console.log(booleanToString(true))
